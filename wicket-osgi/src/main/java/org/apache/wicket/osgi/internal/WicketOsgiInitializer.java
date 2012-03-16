dac25ea4779d604e86b0fa01528b2da6d9bd4a10
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2012-03-16 16:57:20 +0200

    Add logging

--- wicket-osgi/src/main/java/org/apache/wicket/osgi/internal/WicketOsgiInitializer.java
+++ wicket-osgi/src/main/java/org/apache/wicket/osgi/internal/WicketOsgiInitializer.java
@@ -2,19 +2,24 @@ package org.apache.wicket.osgi.internal;
 
 import org.apache.wicket.Application;
 import org.apache.wicket.IInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class WicketOsgiInitializer implements IInitializer {
 
+    private static final Logger LOG = LoggerFactory.getLogger(WicketOsgiInitializer.class);
+
     @Override
-    public void init(Application application) {
+    public void init(final Application application) {
+        LOG.debug("Initializing application '{}' for OSGi", application.getName());
+
         application.getApplicationSettings().setClassResolver(
             new ExtremlyBadUncachingTemporaryDelegatingBundleClassLoader());
     }
 
     @Override
-    public void destroy(Application application) {
-        // TODO Auto-generated method stub
-
+    public void destroy(final Application application) {
+        LOG.debug("Destroying OSGi initializer for application: {}", application.getName());
     }
 
 }
